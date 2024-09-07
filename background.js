80455a0c54561f6dccbb5f336416847e18a2a391
ykamenashi <kamenashi@cloud-ace.jp>
2024-09-07 18:26:36 +0900

    CORSでSecurityErrorが未解決

--- background.js
+++ background.js
@@ -21,22 +21,20 @@ const injectedFunc = (src_jszip, src_FileSaver)=>{
         }).replace(/\//g,'-').replace(/:/g,'').replace(/ /,'_');
     const user_id = document.querySelectorAll('article header a')[0].href.split('@')[1];
     //const img_list = Array.from(document.querySelectorAll('.image .chromatic-ignore img')).map(({src}) => src);
+    const img_tag_list = document.querySelectorAll('.image .chromatic-ignore img');
     const canvas_list = document.querySelectorAll('.image .chromatic-ignore canvas');
     /*
     window.alert(
         `投稿日: ${toukoubi}\n 作者: ${user_id}\n 画像: ${img_list.join('\n')}`
     );
     */
-   /*
-   const spt = document.createElement('script');
-   spt.src = src_jszip;
-   document.head.appendChild(spt);
-   */
    (async () => {
         var zip = new JSZip();
         const cvs_ref = [];
         for( let i=0, max=canvas_list.length; i<max; i++){
+            const img = img_tag_list[i];
             const cvs = canvas_list[i];
+            cvs.getContext('2d').drawImage(img, 0, 0);
             cvs_ref.push(cvs);
         }
         Promise.all(cvs_ref.map(async (cvs)=>{
